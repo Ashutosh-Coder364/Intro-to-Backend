42e092de91e388d0528d11be39a4ea11cdae9901
iamcurious <ashu.coder.1311@gmail.com>
2026-08-08 08:08:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express');
+
+const app = express()
+
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get("/login", (req, res) => {
+    res.send("<h1>Please login at my website</h1>")
+})
+app.get("/twitter", (req, res) => {
+    res.send("hiteshdotcom")
+})
+app.get("/youtube", (req, res) => {
+    res.send("<h2>Drink coffee</h2>")
+})
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`)
+})
